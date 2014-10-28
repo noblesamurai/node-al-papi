246905f8f34e1e69847718b5dfc61770db2c6052
Tim Allen <tim@noblesamurai.com>
2014-10-29 09:36:30 +1100

    Test to prove that async works.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
 var alApi = require('../'),
-    expect = require('expect.js');
-describe('the library', function() {
+    expect = require('expect.js'),
+    async = require('async');
+
+describe('al-papi', function() {
   it('gives unauthorized if so', function(done) {
     alApi.AlConfig('wrong');
     // Make request object to make api calls
@@ -15,17 +17,31 @@ describe('the library', function() {
     }
   });
 
-  it('gives gives success if auth ok', function(done) {
+  describe('when properly authed', function() {
     alApi.AlConfig(process.env.API_KEY);
     // Make request object to make api calls
     var req = new alApi.AlRequest();
-    req.post({'keyword' : 'Centaur Soirée'}, expectations);
 
-    function expectations(response) {
-      expect(response.success).to.be(true);
-      expect(response.statusCode).to.be(200);
-      done();
-    }
+    it('gives gives success if auth ok', function(done) {
+      req.post({'keyword' : 'Centaur Soirée'}, expectations);
+
+      function expectations(response) {
+        expect(response.success).to.be(true);
+        expect(response.statusCode).to.be(200);
+        done();
+      }
+    });
+    it('can have two async requests going', function(done) {
+      async.times(2, function(n, next) {
+        req.post({'keyword' : 'Centaur Soirée'}, function(result) {
+          if (result.success) {
+            next();
+          } else {
+            next(new Error(result.errorMessage));
+          }
+        });
+      }, done);
+    });
   });
 });
 
